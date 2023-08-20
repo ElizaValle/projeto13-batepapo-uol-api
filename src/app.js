03675d2b9019265c5cb113ae1161f0bd928bda78
ElizaValle <beteduvalle@gmail.com>
2023-08-20 10:14:56 -0300

    requisito POST \messages

--- src/app.js
+++ src/app.js
@@ -26,14 +26,12 @@ const schemaMessages = joi.object({
     from: joi.string().required(),
     to: joi.string().required(),
     text: joi.string().required(),
-    type: joi.string().required()
+    type: joi.string().required().valid("message", "private_message")
 })
 
 // endpoints
 app.post("/participants", async (req, res) => {
     const { name } = req.body
-
-    //console.log(name)
 
     const validation = schemaParticpants.validate(req.body, { abortEarly: false })
     if (validation.error) return res.sendStatus(422)
@@ -54,7 +52,7 @@ app.post("/participants", async (req, res) => {
 
         await db.collection("messages").insertOne(message)
 
-        res.status(201).send(message)
+        res.sendStatus(201)
 
     } catch (err) {
         return res.status(500).send(err.message)
@@ -64,6 +62,32 @@ app.post("/participants", async (req, res) => {
 app.get("/participants", async (req, res) => {
     try {
         res.send(await db.collection("participants").find().toArray())
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+})
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const { user } = req.headers
+
+    const validation = schemaMessages.validate({ ...req.body, from: user }, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message)
+        return res.status(422).send(errors)
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({ name: user })
+        if (!participant) return res.sendStatus(422)
+
+        await db.collection("messages").insertOne(
+            { 
+                from: user, to, text, type, time: dayjs().format("HH:mm:ss") 
+            })
+        
+        res.sendStatus(201)
+
     } catch (err) {
         return res.status(500).send(err.message)
     }
